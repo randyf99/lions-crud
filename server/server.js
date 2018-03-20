e10db18971b708a5aa58f4a23f65a9cf1eb6ba69
Randy Ferrer <randfer@gmail.com>
2018-03-20 18:10:49 -0400

    Added updateId function to top of middleware chain

--- server/server.js
+++ server/server.js
@@ -5,13 +5,22 @@ var morgan = require('morgan');
 
 var app = express();
 
+var lions = [];
+var id = 0;
+
+var updateId = function(req, res, next) {
+  if (!req.body.id) {
+    id++;
+    req.body.id = id + '';
+  }
+
+  next();
+};
+
 app.use(morgan('dev'));
 app.use(express.static('client'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-var lions = [];
-var id = 0;
 
 // routes
 app.get('/lions', (req, res) => {
